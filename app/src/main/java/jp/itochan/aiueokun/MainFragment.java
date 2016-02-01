e03a5a4c86b8f36e0ffbdb26670085ef00382f70
Kazunori Jo <itochan315@gmail.com>
2016-02-02 02:10:46 +0900

    Organize imports

--- app/src/main/java/jp/itochan/aiueokun/MainFragment.java
+++ app/src/main/java/jp/itochan/aiueokun/MainFragment.java
@@ -1,10 +1,8 @@
 package jp.itochan.aiueokun;
 
-import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.util.Xml;
 import android.view.LayoutInflater;
